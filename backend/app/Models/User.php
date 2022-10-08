ed78c0692d1f2776141903f06b599524dd626b25
Zeina <zeina@Zeinas-MacBook-Pro.local>
2022-10-08 18:57:52 +0300

    backend: create user model

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -11,19 +11,32 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    protected $dates = ['email_verified_at'];
-
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
         'password',
+        'user_type_id'
     ];
 
     protected $hidden = [
         'password',
     ];
 
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function assignments(){
+        return $this->hasMany(Assignment::class, 'user_id', 'id');
+    }
+
+    public function announcements(){
+        return $this->hasMany(Announcement::class, 'user_id', 'id');
+    }
+
+    public function courses(){
+        return $this->hasMany(Course::class, 'user_id', 'id');
+    }
+
+    public function userHasAssignments(){
+        return $this->hasMany(UserHasAssignment::class, 'user_id', 'id');
+    }
+
 }
