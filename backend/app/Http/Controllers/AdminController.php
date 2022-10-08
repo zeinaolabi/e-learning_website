86064ea64409183392d9a579c23b14a4c660e282
Zeina <zeina@Zeinas-MacBook-Pro.local>
2022-10-08 21:58:46 +0300

    backend: finish add new user api

--- backend/app/Http/Controllers/AdminController.php
+++ backend/app/Http/Controllers/AdminController.php
@@ -27,5 +27,30 @@ class AdminController extends Controller
                 "Users" => $users], 200
         );
     }
-    
+
+    function addUser(Request $request){
+        //Validate all input
+        $validator = Validator::make($request->all(), [
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|min:6',
+            'user_type_id' => 'required|integer'
+        ]);
+
+        //If validation failed, display an error
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 200);
+        }
+
+        $validator->password = Hash::make($request->password);
+
+        //No need to merge the array, just change the password
+        $user = User::create($validator->validated());
+
+        return response()->json([
+            'message' => 'Instructor Successfully Added',
+            'user' => $user
+        ], 201);
+    }
 }
