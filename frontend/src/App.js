371d0196721ece0ce691832ea0c80d48c4c1db98
Zeina <zeina@Zeinas-MacBook-Pro.local>
2022-10-09 20:34:33 +0300

    frontend: add login client

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
 import LoginFrom from './components/login';
+import React, {useState} from 'react';
+import axios from "axios";
+const baseURL = "http://127.0.0.1:8002/api/login";
+// const config = {
+//   headers: Authorization
+// }
 
 function App() {
+  const [user, setUser ] = useState({id: "", token: ""});
+  const [error, setError ] = useState("");
+  
+  const login = (input) => {
+    axios.post(baseURL, input)
+    .then( response => {
+      setUser({
+        id: response.data.user._id,
+        token: "bearer" + response.data.access_token
+      })
+      console.log(user);
+    })
+    .catch((error) =>
+      setError("Invalid Input"));
+
+  }
+
   return (
     <div className="App">
-      <LoginFrom/>
+      <LoginFrom login = {login} error = {error}/>
     </div>
   );
 }
--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 
-const LoginFrom = () => {
+const LoginFrom = ({login, error}) => {
+
+    const [input, setInput] = useState({email:"", password:""});
+
+    const submit = (e) =>{
+        e.preventDefault();
+
+        login(input);
+    }
 
     return(
-        <div className="login_form">
+        <div className="login_form" >
                 <div className="form_content">
                 <h1 class="login_title">Login</h1>
 
-                <input type="text" className="textfield" placeholder="Email"></input>
-                <input type="text" className="textfield" placeholder="Password"></input>
+                <input type="text" className="textfield" placeholder="Email" onChange={(e) =>setInput({...input, email: e.target.value})}></input>
+                <input type="password" className="textfield" placeholder="Password" onChange={(e) =>setInput({...input, password: e.target.value})}></input>
 
-                <button className="login_btn">Login in</button>   
+                <button className="login_btn" onClick={submit}>Login in</button> 
+                {(error != "") ? <span className="error_message">{error}</span> : ""}
             </div>
         </div>
     )
