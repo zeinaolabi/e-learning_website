55c43b1ba38980627633a572600bc78fe60a28ab
Zeina <zeina@Zeinas-MacBook-Pro.local>
2022-10-11 23:20:46 +0300

    frontend: get student assignments

--- frontend/src/components/studentCourses.js
+++ frontend/src/components/studentCourses.js
@@ -28,7 +28,7 @@ function StudentCourses() {
                 course.map((data)=>{
                     return(
                         <Link to="/student_course" state={{ id: data._id, title: data.name }} className="navbar_title">
-                        <Course title={data.name} description={data.description} instructor={(data.user.first_name + data.user.last_name) != null ?  (data.user.first_name + data.user.last_name) : ""}/>
+                            <Course title={data.name} description={data.description} instructor={(data.user.first_name + data.user.last_name) != null ?  (data.user.first_name + data.user.last_name) : ""}/>
                         </Link>
                     )
                 })
