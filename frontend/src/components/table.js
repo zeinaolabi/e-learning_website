8836621ef1a0623b94012c66b0049e32c082e8cf
Zeina <zeina@Zeinas-MacBook-Pro.local>
2022-10-11 09:23:27 +0300

    frontend: finish get students client

--- frontend/src/components/table.js
+++ frontend/src/components/table.js
@@ -26,7 +26,7 @@ function Table({url}) {
                 </tr>
                 {
                     user.map((data)=>{
-                        return(<tr><td>{data.first_name}</td><td>{data.last_name}</td><td>{data.email}</td></tr>)
+                        return(<tr><td>{data.first_name}</td><td>{data.last_name}</td><td>{data.email}</td><td><img src="./../images/update.png"></img></td></tr>)
                     })
                 }
             </tbody>
--- frontend/src/views/AdminPage/studentsPage.js
+++ frontend/src/views/AdminPage/studentsPage.js
@@ -1,13 +1,21 @@
 import React from "react";
 import SideMenu from '../../components/sideMenu';
 import Header from '../../components/header';
+import Table from '../../components/table';
+const baseURL = "http://127.0.0.1:8000/api";
+const getInstructorsAPI = "/get_students";
+
+console.log(baseURL + getInstructorsAPI);
 
 const GetStudentsPage = () => {
 
     return(
         <div class="admin_container">
             <SideMenu />
-            <Header title="Students Page"/>
+            <div class="admin_content">
+                <Header title="Students Page"/>
+                <Table url={baseURL + getInstructorsAPI}/>
+            </div>
         </div>
     )
 }
